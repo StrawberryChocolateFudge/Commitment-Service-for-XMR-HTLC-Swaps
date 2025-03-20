f96bf4ba83392c44e246f4b86048f5deea08a763
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-03-20 21:33:47 +0100

    added getTransferByTxI and view wallet

--- monero.go
+++ monero.go
@@ -14,6 +14,7 @@ import (
 func getClient() *walletrpc.Client {
 	moneroauthuser := os.Getenv("moneroauthuser")
 	moneroauthpass := os.Getenv("moneroauthpass")
+
 	return walletrpc.New(walletrpc.Config{
 		Address: "http://127.0.0.1:18083/json_rpc",
 		Client: &http.Client{
@@ -49,4 +50,42 @@ func checkTxKey(Txid string, TxKey string, Address string) (*walletrpc.CheckTxKe
 	}
 
 	return res, nil
+}
+
+func fromViewKey(restoreHeight uint64, viewKey string, address string, filename string, password string, save_current bool) (*walletrpc.GenerateFromKeysResponse, error) {
+	client := getClient()
+
+	res, err := client.GenerateFromKeys(context.Background(), &walletrpc.GenerateFromKeysRequest{
+		RestoreHeight:   restoreHeight,
+		Filename:        filename,
+		Address:         address,
+		ViewKey:         viewKey,
+		Password:        password,
+		AutosaveCurrent: save_current,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Error when creating view only wallet V%", err)
+	}
+	return res, nil
+}
+
+func handleViewKeyResponse(info string) {
+	// TODO: handle the info to see if the wallet was generated
+}
+
+func getTransfer(txid string) (*walletrpc.GetTransferByTxidResponse, error) {
+	client := getClient()
+	res, err := client.GetTransferByTxid(context.Background(), &walletrpc.GetTransferByTxidRequest{
+		Txid: txid,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("Error when getting transfer v%", err)
+	}
+
+	return res, nil
+}
+
+func handleTransferDetails() {
+	//TODO:handle how the transfer was done
 }
